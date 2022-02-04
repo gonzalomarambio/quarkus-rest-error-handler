417479c848cc5907485bcdbffda27345881f4d0c
Gonzalo Marambio <gonzalo.marambio.gutierrez@gmail.com>
2022-02-04 17:20:35 -0300

    Add files via upload
    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Quarkus Rest Error Handler Exception
+
+Esta prueba de concepto, encapsula los distintas excepciones que se envian a un servicio rest
+
+## Versiones
+
+* Java 11
+* Quarkus 2.7.0.Final
---
+++ src/test/java/com/gonkables/NativeexampleIT.java
@@ -0,0 +1,9 @@
+package com.gonkables;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeexampleIT extends exampleTest {
+
+    // Execute the same tests but in native mode.
+}
